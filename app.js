9c4aee141d829e6a71cdadae611e559257a2c20b
Joams Mogro <joamsmg@gmail.com>
2022-03-23 09:26:18 -0400

    menu repetido con Promise

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require('colors');
 
-const { mostrarMenu } = require('./helpers/mensajes');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 const main =  async()=>{
-    console.log('Hola mundo');
-    mostrarMenu();
+
+    let opt='';
+
+    do{
+        opt = await mostrarMenu();
+        if(opt!=='0') await pausa();
+    }while(opt!=='0')
 }
 
 main();
