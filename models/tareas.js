b6165b095d50af459e211c8952af00f92361064b
Joams Mogro <joamsmg@gmail.com>
2022-03-24 12:21:55 -0400

    convirtiendo objetos a array (listado)

--- models/tareas.js
+++ models/tareas.js
@@ -6,6 +6,14 @@ class Tareas {
     constructor(){
         this._listado = {};
     }
+    get listadoArr(){
+        const listado = [];
+        Object.keys(this._listado).forEach(key =>{
+            const tarea = this._listado[key];
+            listado.push(tarea)
+        })
+        return listado;
+    }
     crearTarea(desc=''){
         const tarea = new Tarea(desc);
         this._listado[tarea.id] = tarea;
